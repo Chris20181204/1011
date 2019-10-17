b263706fdaa55026dd66de500d54def7500995db
s2013623 <s2013623@gmail.com>
2019-10-17 10:01:10 +0800

    git init

---
+++ js/all.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+//  $('.wrap .bars').click(function(){
+//     $('ul').toggle();
+//     $('ul ul').css('display','none');
+//  });
+
+//  $('ul li').click(function(){
+//     $('ul ul').slideUp();
+//     $(this).find('ul').slideToggle();
+//  });
+
+
+//     $('.menu > li > a').hover(function(event){
+//         event.preventDefault();
+//         $(this).parent().siblings().find('ul').hide();
+//         $(this).parent().find('ul').toggle(2000);
+//         $(this).parent().find('ul').show();
+//     });
+//     $('.menu > li > a').click(function(event){
+//         event.preventDefault();
+        
+
+//     });
+
+//    $('.top a').click(function(event){
+//     event.preventDefault();
+//     $('html , body').animate({
+//         scrollTop:0,
+//     },10000);
+//    });
+
+});
+    
+
+ 
+
+
+
